56309e4027dedd738e3abe5ca1a889cf621da388
Karl <kfetherston@gmail.com>
2021-08-18 15:26:52 +0100

    • Hilt config

--- app/src/main/java/com/kfeth/androidcleanarchitecture/domain/interaction/weather/GetWeatherUseCaseImpl.kt
+++ app/src/main/java/com/kfeth/androidcleanarchitecture/domain/interaction/weather/GetWeatherUseCaseImpl.kt
@@ -1,8 +1,10 @@
 package com.kfeth.androidcleanarchitecture.domain.interaction.weather
 
 import com.kfeth.androidcleanarchitecture.domain.repository.WeatherRepository
+import javax.inject.Inject
 
-class GetWeatherUseCaseImpl(private val weatherRepository: WeatherRepository) : GetWeatherUseCase {
+class GetWeatherUseCaseImpl @Inject constructor(private val weatherRepository: WeatherRepository) :
+    GetWeatherUseCase {
 
     override suspend operator fun invoke(location: String) =
         weatherRepository.getWeatherForLocation(location)
