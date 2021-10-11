5971ba2f38a02bac32c13163b9ace908f3aa984a
Karl <kfetherston@gmail.com>
2021-10-11 18:26:44 +0100

    • Removing unused layouts + fragments (replaced with Compose)

--- app/src/main/java/com/kfeth/template/MainActivity.kt
+++ app/src/main/java/com/kfeth/template/MainActivity.kt
@@ -1,32 +1,17 @@
 package com.kfeth.template
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-//import androidx.navigation.fragment.NavHostFragment
-//import androidx.navigation.ui.AppBarConfiguration
-//import androidx.navigation.ui.navigateUp
-//import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.kfeth.template.ui.NewsApp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
-//    private lateinit var appBarConfiguration: AppBarConfiguration
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-//        val navHostFragment =
-//            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-//        val navController = navHostFragment.navController
-//        appBarConfiguration = AppBarConfiguration(navController.graph)
-//        setupActionBarWithNavController(navController, appBarConfiguration)
+        setContent {
+            NewsApp()
+        }
     }
-
-//    override fun onSupportNavigateUp(): Boolean {
-//        val navController = findNavController(R.id.nav_host_fragment)
-//        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-//    }
 }
